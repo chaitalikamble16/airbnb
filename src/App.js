17396164fcc36733a9c9b3bab8c9bcc370d6ae6e
chaitalikamble16 <chaitalikamble16@gmail.com>
2024-02-18 14:27:51 +0530

    commit 2 in branch 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Chaitali Edit <code>src/App.js</code> and save to reload.
+          Chaitali 11 Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
